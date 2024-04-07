3b702655987232277aa8aeac195921a1f27a8b68
Olta Olloni <oltaxx@gmail.com>
2024-04-07 15:08:59 +0200

    small changes

--- Villa/properties.php
+++ Villa/properties.php
@@ -78,10 +78,8 @@ https://templatemo.com/tm-591-villa-agency
                     <ul class="nav">
                       <li><a href="index.php">Home</a></li>
                       <li><a href="properties.php" class="active">Properties</a></li>
-          
-                     
                       <li><a href="lease.php">Lease your villa</a></li>
-
+                      <li><a href="mybookings.php">My Bookings</a></li>
                       <li><a href="login.php">Log in | Sign up</a></li>
                       
                   </ul>   
