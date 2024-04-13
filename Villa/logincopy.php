91c141e92865db2c7056fad2afb23d4165f6c4fb
Olt Vrella <oltvrella@gmail.com>
2024-04-13 10:25:09 +0200

    Do mini ndryshime

--- Villa/logincopy.php
+++ Villa/logincopy.php
@@ -249,6 +249,9 @@ form .btn input[type="submit"] {
             <input type="text" name="email" placeholder="Email Address" required>
             </div>
             <div class="field">
+            <input type="text" name="username" placeholder="Username" required>
+            </div>
+            <div class="field">
             <input type="password" name="password" placeholder="Password" required>
             </div>
             <div class="field">
--- Villa/signup.php
+++ Villa/signup.php
@@ -2,15 +2,25 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password']; 
+    $username=$_POST['username'];
 
- 
-    $data = $email . ',' . $password . "\n";
+    $file_content=file("users.txt",FILE_IGNORE_NEW_LINES);
+    $email_exist=false;
+    foreach($file_content as $line){
+        $params=explode("~",$line);
+        if($params[0]===$email){
+            $email_exist=true;
+            break;
+        }
+    }
 
-
-    file_put_contents("users.txt", $data, FILE_APPEND);
-
-
-    echo "Signup successful. <a href='logincopy.php'>Login here</a>";
+if (!$email_exist) {
+     $data = implode("~",array($email,$username,hash("sha256",$password)));
+    file_put_contents("users.txt", $data."\n", FILE_APPEND);
+    echo "Signup successful. <a href='logincopy.php'>Login here</a>";}
+    else{
+        echo "You already have an account. <a href='logincopy.php'>Login here</a>";
+    }
 } else {
 
     header("Location: logincopy.php");
