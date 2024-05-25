aacbab08d433658dc0c82daa7ccbba443b29e34b
oraosmanii <oraosmani@gmail.com>
2024-05-25 16:45:06 +0200

    db setup update

--- Villa/database_setup.sql
+++ Villa/database_setup.sql
@@ -1,27 +1,5 @@
 CREATE DATABASE villadb;
 
--- Users table
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    salt VARCHAR(255) NOT NULL
-);
-
--- Properties Table
-CREATE TABLE properties (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    country VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
-    date DATE NOT NULL,
-    image VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    bedrooms INT NOT NULL,
-    bathrooms INT NOT NULL,
-    area INT NOT NULL,
-    type VARCHAR(50) NOT NULL
-);
 
 -- Insert values into the properties table
 INSERT INTO properties (country, city, date, image, price, bedrooms, bathrooms, area, type) VALUES
@@ -34,28 +12,3 @@ INSERT INTO properties (country, city, date, image, price, bedrooms, bathrooms, 
 ('Croatia', 'Split', '2024-04-15', './assets/images/property-01.jpg', 500, 5, 5, 270, 'villa'),
 ('Slovenia', 'Ljubljana', '2024-04-15', './assets/images/property-02.jpg', 420, 2, 3, 115, 'penthouse'),
 ('Germany', 'Berlin', '2024-04-15', './assets/images/property-04.jpg', 200, 3, 4, 120, 'villa');
-
--- bookings table
-CREATE TABLE bookings (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    property_id INT NOT NULL,
-    arrival_date DATE NOT NULL,
-    departure_date DATE NOT NULL,
-    phone_number VARCHAR(20) NOT NULL,
-    payment_method VARCHAR(255) NOT NULL,
-    comment TEXT,
-    total_price DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (property_id) REFERENCES properties(id)
-);
--- ratings
-CREATE TABLE ratings (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    property_id INT NOT NULL,
-    rating INT NOT NULL,
-    UNIQUE KEY unique_rating (user_id, property_id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (property_id) REFERENCES properties(id)
-);
