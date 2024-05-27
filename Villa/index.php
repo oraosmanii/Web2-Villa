28c4f7f42f566446b4ecac3066d106dad6e5a1c2
oraosmanii <oraosmani@gmail.com>
2024-05-27 03:09:35 +0200

    CONTACT FORM EMAIL

--- Villa/index.php
+++ Villa/index.php
@@ -407,7 +407,7 @@ https://templatemo.com/tm-591-villa-agency
   </div>
 
 
-  <!-- <div class="contact section">
+  <div class="contact section">
     <div class="container">
       <div class="row">
         <div class="col-lg-4 offset-lg-4">
@@ -418,9 +418,9 @@ https://templatemo.com/tm-591-villa-agency
         </div>
       </div>
     </div>
-  </div> -->
+  </div>
 
-  <!-- <div class="contact-content">
+  <div class="contact-content">
     <div class="container">
       <div class="row">
         <div class="col-lg-7">
@@ -428,25 +428,25 @@ https://templatemo.com/tm-591-villa-agency
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12469.776493332698!2d-80.14036379941481!3d25.907788681148624!2m3!1f357.26927939317244!2f20.870722720054623!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x88d9add4b4ac788f%3A0xe77469d09480fcdb!2sSunny%20Isles%20Beach!5e1!3m2!1sen!2sth!4v1642869952544!5m2!1sen!2sth" width="100%" height="500px" frameborder="0" style="border:0; border-radius: 10px; box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.15);" allowfullscreen=""></iframe>
           </div>
           <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-5">
               <div class="item phone">
                 <img src="assets/images/phone-icon.png" alt="" style="max-width: 52px;">
-                <h6>010-020-0340<br><span>Phone Number</span></h6>
+                <h6>045-117-755<br><span>Phone Number</span></h6>
               </div>
             </div>
-            <div class="col-lg-6">
+            <div class="col-lg-7">
               <div class="item email">
                 <img src="assets/images/email-icon.png" alt="" style="max-width: 52px;">
-                <h6>info@villa.co<br><span>Business Email</span></h6>
+                <h6>villaweb2324@gmail.com<br><span>Business Email</span></h6>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-5"> -->
+        <div class="col-lg-5"> 
 
-                                <!-- <?php
+                                <?php
 
-                        //session_start();
+                        
 
                         function countFormSubmissions() {
                             
@@ -464,61 +464,44 @@ https://templatemo.com/tm-591-villa-agency
                             //echo "This form is submitted " . $form_submission_count . " times!";
                         }
                         ?>
-          <form id="contact-form" action="" method="POST">
-            <div class="row">
-              <div class="col-lg-12">
-                <fieldset>
-                  <label for="name">Full Name</label>
-                  <input type="name" name="name" id="name" placeholder="Your Name..." autocomplete="on" required>
-                </fieldset>
-              </div>
-              <div class="col-lg-12">
-                <fieldset>
-                  <label for="email">Email Address</label>
-                  <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your E-mail..." required="">
-                  <?php 
-          if (isset($_POST["message"])) {
-            $email = $_POST["email"];
-            $email_pattern = '/^[a-zA-Z][a-zA-Z0-9_]*@[a-zA-Z.]+$/';
-          if (preg_match($email_pattern, $email)) {
-              echo "<script>alert('Thanks for contacting us!');
-              window.location.href = '#sub-header';
-              </script>";
-            //header("Location: index.php");
-        } else {
-            $error_message= "Invalid email address!";
-            echo "
-           <script> window.location.href = '#contact-form';
-            </script>" . "<p style='color: red'>$error_message</p><br>";
-            
-        }
-      }
-          ?>
-                </fieldset>
-              </div>
-              <div class="col-lg-12">
-                <fieldset>
-                  <label for="subject">Subject</label>
-                  <input type="subject" name="subject" id="subject" placeholder="Subject..." autocomplete="on" >
-                </fieldset>
-              </div>
-              <div class="col-lg-12">
-                <fieldset>
-                  <label for="message">Message</label>
-                  <textarea name="message" id="message" placeholder="Your Message"></textarea>
-                </fieldset>
-              </div>
-              <div class="col-lg-12">
-                <fieldset>
-                  <button type="submit" id="form-submit" name="message" class="orange-button">Send Message</button>
-                </fieldset>
-              </div>
-            </div>
-          </form>
+         <form id="contact-form" action="process_contact_form.php" method="POST">
+    <div class="row">
+        <div class="col-lg-12">
+            <fieldset>
+                <label for="name">Full Name</label>
+                <input type="name" name="name" id="name" placeholder="Your Name..." autocomplete="on" required>
+            </fieldset>
+        </div>
+        <div class="col-lg-12">
+            <fieldset>
+                <label for="email">Email Address</label>
+                <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your E-mail..." required="">
+            </fieldset>
+        </div>
+        <div class="col-lg-12">
+            <fieldset>
+                <label for="subject">Subject</label>
+                <input type="subject" name="subject" id="subject" placeholder="Subject..." autocomplete="on">
+            </fieldset>
+        </div>
+        <div class="col-lg-12">
+            <fieldset>
+                <label for="message">Message</label>
+                <textarea name="message" id="message" placeholder="Your Message"></textarea>
+            </fieldset>
+        </div>
+        <div class="col-lg-12">
+            <fieldset>
+                <button type="submit" id="form-submit" name="submit" class="orange-button">Send Message</button>
+            </fieldset>
+        </div>
+    </div>
+</form>
+
         </div>
       </div>
-    </div> -->
-  <!-- </div> -->
+    </div>
+
 
   <footer>
     <div class="container">
@@ -530,8 +513,7 @@ https://templatemo.com/tm-591-villa-agency
     </div>
   </footer>
 
-  <!-- Scripts -->
-  <!-- Bootstrap core JavaScript -->
+
   <script>
         function handleClick(choice) {
             var box = document.getElementById('ratingBox');
